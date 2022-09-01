58461655050550526b5500debe7e5a73768f44b4
Sergienko Anton <sergienko.anton.v@gmail.com>
2022-09-01 23:44:39 +0300

    Enviroment done. Telegrams api connected

---
+++ src/main/java/io/proj3ct/SpringDemoBot1234/SpringDemoBot1234Application.java
@@ -0,0 +1,13 @@
+package io.proj3ct.SpringDemoBot1234;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoBot1234Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoBot1234Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/proj3ct/SpringDemoBot1234/SpringDemoBot1234ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.proj3ct.SpringDemoBot1234;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoBot1234ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
